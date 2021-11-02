7623d2de6644304fe794db9501c87dd6dd300fcd
RealSauceDoctor <51185529+RealSauceDoctor@users.noreply.github.com>
2021-11-02 15:41:11 -0600

    three times three

--- js/r3nderer.js
+++ js/r3nderer.js
@@ -7,20 +7,11 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-
 //creating the objects
-
-    //const loader = new GLTFLoader();
-    //loader.load('models/monkey.glb', function(gltf) {
-    //    scene.add(gltf.scene);
-    //}, undefined, function(error) {
-    //    console.error(error);
-    //});
-
 const geometry = new THREE.BoxGeometry(); //creating the geometry
-const material = new THREE.MeshBasicMaterial({
+const material = new THREE.MeshPhongMaterial({
     color: 0x660000
-}); //creating the material
+}); //creating the material, MeshBasicMaterial don't catch light
 const cube = new THREE.Mesh(geometry, material); //creating the mesh
 scene.add(cube); //adding mesh to scene
 
@@ -28,14 +19,23 @@ scene.add(cube); //adding mesh to scene
 const gizmo = new THREE.AxesHelper(2);
 scene.add(gizmo); //x=red, y=green, z=blue
 
-//camera.position.z = 5;
-camera.position.set(0, 2, 5);//XYZ
+//lighting
+const lampColor1 = 0xFFFFFF;
+const lampIntensity1 = 2;
+const lamp1 = new THREE.DirectionalLight(lampColor1, lampIntensity1);
+lamp1.position.set(1, 2, 0);
+scene.add(lamp1);
+
+//camera
+camera.position.set(0, 2, 5); //XYZ
 camera.lookAt(cube.position);
 
+//rendering
 function animate(time) {
     time *= 0.001;
 
-    cube.rotation.x=time;
+    cube.rotation.set(time, time, time);
+    cube.position.set(Math.cos(time), Math.sin(time), Math.cos(time / 2))
 
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
